7c04ce813aef4c24bd9a7ca2162d579ee053edba
Andres Castro <andresncastro12@gmail.com>
2025-10-16 17:57:38 -0400

    optimized preprocess_data file and created a practice file for further coding

--- backend/scripts/preprocess_data.py
+++ backend/scripts/preprocess_data.py
@@ -1,68 +1,79 @@
-import torch
-import numpy as np
+
 import os
 import cv2 as cv
 from mtcnn import MTCNN
-from mtcnn.utils.images import load_image
 import argparse
-PROCESSED_REAL = '../data/processed_data/real'
-PROCESSED_FAKE = '../data/processed_data/fake'
+from tqdm import tqdm
+PROCESSED_REAL = '../data/processed_data/real/'
+PROCESSED_FAKE = '../data/processed_data/fake/'
+mtcnn = MTCNN(device="CPU:0")
+
+def process_single_frame(video : str, dataset : str, dataset_type : int):
+  video_path = os.path.join(dataset, video)
+  current_frame = 0
+  cv_video = cv.VideoCapture(video_path)
+  frame_step = int(cv_video.get(cv.CAP_PROP_FRAME_COUNT) / 20)
+
+  if frame_step == 0:
+    return
+
+  for current_frame in range(20):
+    
+    frame_index = current_frame * frame_step
+    cv_video.set(cv.CAP_PROP_POS_FRAMES, frame_index)
+    success, frame = cv_video.read()
+
+    if success == False:
+      return
+
+    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    result = mtcnn.detect_faces(image)
+
+    if result:
+      box = result[0]['box']
+      x = box[0]
+      y = box[1]
+      w = box[2]
+      h = box[3]
+      face_frame = frame[y : y + h, x : x + w]
+      
+      resized_face_frame = cv.resize(face_frame, (256, 256))
+
+      if dataset_type == 0:
+        filename = f"{video}_frame_{current_frame}_dataset_real.png"
+        store_path = os.path.join(PROCESSED_REAL, filename)
+        cv.imwrite(store_path, resized_face_frame)
+      
+      else:
+        filename = f"{video}_frame_{current_frame}_dataset_fake.png"
+        store_path = os.path.join(PROCESSED_FAKE, filename)
+        cv.imwrite(store_path, resized_face_frame)
+
 
 def obtain_face_frames(dataset_path : str):
   try:
-    mtcnn = MTCNN(device="CPU:0")
     datasets = []
     datasets.append(dataset_path + 'real')
     datasets.append(dataset_path + 'fake')
 
     for dataset in datasets:
       videos = os.listdir(dataset)
-      for video in videos:
-        current_frame = 0
-        video_path = os.path.join(dataset, video)
-        cv_video = cv.VideoCapture(video_path)
-        frame_step = int(cv_video.get(cv.CAP_PROP_FRAME_COUNT) / 20)
 
-        if frame_step == 0:
-          continue
-
-        for current_frame in range(20):
+      try:
+        for video in tqdm(videos):
           
-          frame_index = current_frame * frame_step
-          cv_video.set(cv.CAP_PROP_POS_FRAMES, frame_index)
-          success, frame = cv_video.read()
+          if dataset == datasets[0]:
+            process_single_frame(video, dataset, 0)
+          else:
+            process_single_frame(video, dataset, 1)
 
-          if success == False:
-            break
-
-          image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-          result = mtcnn.detect_faces(image)
-
-          if result:
-            box = result[0]['box']
-            x = box[0]
-            y = box[1]
-            w = box[2]
-            h = box[3]
-            face_frame = frame[y : y + h, x : x + w]
-            
-            resized_face_frame = cv.resize(face_frame, (256, 256))
-
-            if dataset == datasets[0]:
-              filename = f"{video}_frame_{current_frame}_dataset_real.png"
-              store_path = os.path.join(PROCESSED_REAL, filename)
-              cv.imwrite(store_path, resized_face_frame)
-            
-            else:
-              filename = f"{video}_frame_{current_frame}_dataset_fake.png"
-              store_path = os.path.join(PROCESSED_FAKE, filename)
-              cv.imwrite(store_path, resized_face_frame)
-          else: continue
+      except Exception as e:
+        print(e)
+        exit()
 
   except Exception as e:
-    print(f"{dataset_path} does not exist")
+    print(e)
     exit()
-
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="finds the required dataset")
